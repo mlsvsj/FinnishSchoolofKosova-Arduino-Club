274db6c9a3c48c1a03f1d6876a60947e780056a4
Melisa Vasija <100197978+mlsvsj@users.noreply.github.com>
2025-11-29 17:31:36 +0100

    Update arduino_club_project_1_1.ino

--- arduino_club_project_1_1.ino
+++ arduino_club_project_1_1.ino
@@ -1,15 +1,17 @@
-// C++ code
-//
-int led=12;
-void setup()
-{
+// Definojmë pinin ku lidhet LED-i
+int led = 12;  
+
+void setup() {
+  // Vendosim pinin e LED-it si OUTPUT (për ta ndezur dhe fikur)
   pinMode(led, OUTPUT);
 }
 
-void loop()
-{
-  digitalWrite(led, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(led, LOW);
-  delay(200); // Wait for 200 millisecond(s)
+void loop() {
+  // Ndezim LED-in
+  digitalWrite(led, HIGH);   
+  delay(1000); // Presim 1000 milisekonda (1 sekondë) që LED-i të qëndrojë ndezur
+
+  // Fikim LED-in
+  digitalWrite(led, LOW);    
+  delay(200); // Presim 200 milisekonda (0.2 sekonda) që LED-i të qëndrojë fikur
 }
